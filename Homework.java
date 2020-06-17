5f325a71287ecc76a8a6226912440f4f89b0523b
jiayu <1847438558@qq.com>
2020-06-17 11:01:59 +0800

    company

---
+++ Homework.java
@@ -0,0 +1,10 @@
+public class java {
+    public static void main(String[] args) {
+        //在家开发
+        private String name;
+	private String content;
+	//在公司开发
+	//private String id;
+    }
+}
+
